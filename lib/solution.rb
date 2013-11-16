9222a59934ae6fea93ce645ab32117ae0c8be14d
Kerri Miller <kerrizor@kerrizor.com>
2013-11-16 01:07:06 -0800

    process STDIN as well as files

--- lib/solution.rb
+++ lib/solution.rb
@@ -3,19 +3,12 @@ require_relative 'text_processor'
 require_relative 'phrase_counter'
 
 phrase_counter = PhraseCounter.new
+text = ARGF.read
 
-ARGV.each do |filename|
-  text = ''
+if text.empty?
+  puts "Sorry, we couldn't find any content to process."
+else
+  phrase_counter.count_phrases(text)
 
-  if File.exists?(filename)
-    file = File.open(filename)
-
-    file.each do |line|
-      text << line.strip
-    end
-  end
-
-  phrase_counter.count_phrases(text) unless text == ''
+  puts phrase_counter.results_table
 end
-
-puts phrase_counter.results_table
