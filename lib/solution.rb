a83837ec8e71ac2dfc84c6685e1e3048711c8be8
Kerri Miller <kerrizor@kerrizor.com>
2013-11-25 20:04:25 -0800

    adds ability to gracefully crash out of infinite loops when processing supermassive files

--- lib/solution.rb
+++ lib/solution.rb
@@ -3,8 +3,14 @@ require_relative 'phrase_counter'
 
 phrase_counter = PhraseCounter.new
 
+interrupted = false
+trap('INT') { interrupted = true }
+
+puts 'Press ^C to exit'
+
 puts Benchmark.measure {
   ARGF.each_line do |line|
+    break if interrupted
 
     phrase_counter.process_line(line)
 
@@ -13,7 +19,7 @@ puts Benchmark.measure {
 }
 
 if phrase_counter.phrases.empty?
-  puts "Sorry, we couldn't find any content to process."
+  puts 'Sorry, we couldn\'t find any content to process.'
 else
   puts phrase_counter.results_table
 end
